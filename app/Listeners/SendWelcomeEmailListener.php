abe375f2038230a9d8daf955f8fb3c5dc219973c
NguyenManh2442 <nguyenmanh25111999m@gmail.com>
2020-11-02 20:15:30 +0700

    send email with EmailListenner

--- app/Listeners/SendWelcomeEmailListener.php
+++ app/Listeners/SendWelcomeEmailListener.php
@@ -4,8 +4,10 @@ namespace App\Listeners;
 
 use App\Events\CreatedUser;
 use App\Jobs\SendEmail;
+use App\Mail\WelcomeUser;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
 
 class SendWelcomeEmailListener implements ShouldQueue
 {
@@ -27,6 +29,6 @@ class SendWelcomeEmailListener implements ShouldQueue
      */
     public function handle(CreatedUser $event)
     {
-        SendEmail::dispatch($event->user);
+        Mail::to($event->user['mail'])->send(new WelcomeUser($event->user['name']));
     }
 }
